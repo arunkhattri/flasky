962acdfc76eb2e4cbbc8874a3b31294a5d4b4761
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-10-20 16:01:10 +0530

    templates, variable filters

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    return render_template("index.html")
 
 
 # dynamic routes
 @app.route("/user<name>")
 def user(name):
-    return "<h1>Hello, {}</h1>".format(name)
+    return render_template("user.html", name=name)
