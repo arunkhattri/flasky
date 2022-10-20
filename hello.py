2725efb2035639055060cb68da66a3c0f85ddae1
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-10-20 23:20:07 +0530

    wtf-form

--- hello.py
+++ hello.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 from datetime import datetime
 from flask import Flask, render_template
-from flask_moment import Moment, moment
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config[
+    "SECRET_KEY"
+] = "I guess I can go anywhere I want. If only I knew where to go"
 moment = Moment(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template(
+        "index.html", current_time=datetime.utcnow(), form=form, name=name
+    )
 
 
 # dynamic routes
